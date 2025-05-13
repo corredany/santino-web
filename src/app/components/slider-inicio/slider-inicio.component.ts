908e35ab8342b1529fb37c4851b7bcbc3db00a18
Arturo <correosdany@outlook.com>
2025-05-13 14:05:10 -0600

    Slider inicio

---
+++ src/app/components/slider-inicio/slider-inicio.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-inicio',
+  imports: [],
+  templateUrl: './slider-inicio.component.html',
+  styleUrl: './slider-inicio.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SliderInicioComponent { }
--- src/app/pages/inicio/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio/inicio.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SliderInicioComponent } from "../../../components/slider-inicio/slider-inicio.component";
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [SliderInicioComponent],
   templateUrl:'./inicio.component.html',
   styleUrl: './inicio.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
