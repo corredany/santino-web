7d46b286e041625968c0da5c883ea132ff7a5719
Ferbox44 <Ferbax03@outlook.com>
2025-05-13 21:46:23 -0600

    Maquetado boton cotiza ahora

---
+++ src/app/components/shared/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+@Component({
+  selector: 'app-button',
+  imports: [RouterModule],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+}
--- src/app/pages/inicio/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio/inicio.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ButtonComponent } from '../../../components/shared/button/button.component';
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [
+    ButtonComponent
+  ],
   templateUrl:'./inicio.component.html',
   styleUrl: './inicio.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
