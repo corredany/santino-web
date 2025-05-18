c464b2000c677d4c20316ad5d16a88f3cc1d95c3
Ferbox44 <Ferbax03@outlook.com>
2025-05-18 14:59:37 -0600

    Slider marcas

--- src/app/components/slider-inicio/slider-inicio.component.ts
+++ src/app/components/slider-inicio/slider-inicio.component.ts
@@ -1,10 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit, ElementRef, ViewChild, AfterViewInit,  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselModule } from 'primeng/carousel';
-import { TagModule } from 'primeng/tag';
 import { ButtonModule } from 'primeng/button';
-// Registra los elementos de Swiper (esto también puede hacerse en el módulo principal)//
-//register();
 
 @Component({
   selector: 'app-slider-inicio',
@@ -16,7 +13,6 @@ import { ButtonModule } from 'primeng/button';
   ],
   templateUrl: './slider-inicio.component.html',
   styleUrls: ['./slider-inicio.component.css'],
-  //schemas: [CUSTOM_ELEMENTS_SCHEMA],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SliderInicioComponent implements OnInit {
@@ -33,8 +29,13 @@ export class SliderInicioComponent implements OnInit {
   ngOnInit() {
     this.responsiveOptions = [
       {
-        breakpoint: '1400px',
+        breakpoint: '1920px',
         numVisible: 3,
+        numScroll: 1
+      },
+      {
+        breakpoint: '1400px',
+        numVisible: 2,
         numScroll: 1
       },
       {
@@ -46,58 +47,13 @@ export class SliderInicioComponent implements OnInit {
         breakpoint: '768px',
         numVisible: 1,
         numScroll: 1
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
       }
     ];
   }
 }
 
-// export class SliderInicioComponent implements OnInit, AfterViewInit {
-//   // Referencia al contenedor Swiper
-//   @ViewChild('swiper') swiperRef!: ElementRef<SwiperContainer>;
-  
-//   // Datos para los slides (podrías moverlos a un servicio si fuera necesario)
-//   slides = [
-//     { image: 'img/img-1.jpg', name: 'Cocina'},
-//     { image: 'img/img-2.jpg', name: 'Closets'},
-//     { image: 'img/img-3.jpg', name: 'Vestidores'},
-//     { image: 'img/img-4.jpg', name: 'Muebles de Baño'},
-//     { image: 'img/img-5.jpg', name: 'Diseño de interiores'},
-//   ];
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-//   ngAfterViewInit(): void {
-//     // Opciones de configuración del Swiper
-//     const swiperParams = {
-//       loop: true,
-//       grabCursor: true,
-//       spaceBetween: 20,
-//       pagination: {
-//         clickable: true,
-//         dynamicBullets: true
-//       },
-//       navigation: true,
-      
-//       breakpoints: {
-//         0: {
-//           slidesPerView: 1
-//         },
-//         800: {
-//           slidesPerView: 2
-//         },
-//         1024: {
-//           slidesPerView: 3
-//         },
-//       }
-//     };
-
-//     // Aplica las opciones e inicializa
-//     Object.assign(this.swiperRef.nativeElement, swiperParams);
-//     this.swiperRef.nativeElement.initialize();
-//   }
-
-  
-// }
--- src/app/pages/inicio/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio/inicio.component.ts
@@ -2,12 +2,14 @@ import { ChangeDetectionStrategy, Component, AfterViewInit, ElementRef, ViewChil
 import { SliderInicioComponent } from "../../../components/slider-inicio/slider-inicio.component";
 import { ButtonComponent } from '../../../components/shared/button/button.component';
 import { ImageCompareModule } from 'primeng/imagecompare';
+import { SliderMarcasComponent } from '../../../components/slider-marcas/slider-marcas.component';
 @Component({
   selector: 'app-inicio',
   imports: [
     SliderInicioComponent, 
     ButtonComponent, 
     ImageCompareModule, 
+    SliderMarcasComponent
   ],
   templateUrl:'./inicio.component.html',
   styleUrl: './inicio.component.css',
